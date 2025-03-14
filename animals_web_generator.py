e863c1d5c6379f8a3b599d52ebbe2915511e3ef5
leezter <66684146+leezter@users.noreply.github.com>
2025-03-15 00:48:44 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,27 +6,29 @@ def load_data(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         return json.load(file)
 
-
 animals_data = load_data("animals_data.json")
 
 animals_info = '<ul class="cards">\n'
 
+
 for animal in animals_data:
     animals_info += '    <li class="cards__item">\n'
-    animals_info += f"        <strong>Name:</strong> {animal['name']}<br/>\n"
+    animals_info += f'        <div class="card__title">{animal["name"]}</div>\n'
+    animals_info += '        <p class="card__text">\n'
 
     if "diet" in animal["characteristics"]:
-        animals_info += f"        <strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
+        animals_info += f'            <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
 
     if "locations" in animal and animal["locations"]:
-        animals_info += f"        <strong>Location:</strong> {animal['locations'][0]}<br/>\n"
+        animals_info += f'            <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
 
     if "type" in animal["characteristics"]:
-        animals_info += f"        <strong>Type:</strong> {animal['characteristics']['type']}<br/>\n"
+        animals_info += f'            <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
 
-    animals_info += "    </li>\n"  
+    animals_info += '        </p>\n'  
+    animals_info += '    </li>\n'  
 
-animals_info += "</ul>\n" 
+animals_info += "</ul>\n"  
 
 with open("animals_template.html", "r", encoding="utf-8") as file:
     html_content = file.read()
@@ -36,4 +38,5 @@ html_content = html_content.replace("__REPLACE_ANIMALS_INFO__", animals_info)
 with open("animals.html", "w", encoding="utf-8") as file:
     file.write(html_content)
 
-print("? animals.html has been successfully updated with correct serialization!")
+# animals.html has now been updated with improved card item")
+
